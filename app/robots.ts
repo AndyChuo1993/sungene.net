3b56a3c1933462dc3b563269f9ac8bff6eed1cad
Andy <andy@sungenelite.com>
2026-04-07 15:38:04 +0800

    seo: harden robots rules against wp and bot paths

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,17 @@ export default async function robots(): Promise<MetadataRoute.Robots> {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: [
+          '/api/',
+          '/_next/',
+          '/wp-admin',
+          '/wp-content',
+          '/wp-includes',
+          '/wp-json',
+          '/xmlrpc.php',
+          '/wp-login.php',
+          '/wp-cron.php',
+        ],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
