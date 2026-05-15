7700bf6b0d692a1d13f85b1474b401a731a1a37a
deploy <deploy@sungene.net>
2026-05-15 20:14:32 +0800

    chore: gitignore .next.previous-static + .next.staging

--- .next.previous-static/V0xdRWorZsMzTJzTBK7En/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[lang]\u002Fprivacy","\u002F[lang]\u002Fresources","\u002F[lang]\u002Fresources\u002F[slug]","\u002F[lang]\u002Fresources\u002Froute\u002F[slug]","\u002F[lang]\u002Fsourcing","\u002F[lang]\u002Fsourcing\u002F[category]","\u002F[lang]\u002Fterms"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
